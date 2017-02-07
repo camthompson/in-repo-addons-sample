e9bbda46fb8be4ef46bcf3df117f11858c3b42f4
Cam Thompson <cam@camthompson.com>
2017-02-07 09:45:56 -0500

    Rewrite bower imports as in-repo addons

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,23 +1,8 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-var pickFiles = require('broccoli-static-compiler');
-var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = function(defaults) {
-  var app = new EmberApp({
-    sassOptions: {
-      includePaths: [
-        'bower_components/bootstrap-sass-official/assets/stylesheets'
-      ]
-    }
-  });
+  var app = new EmberApp();
 
-  var bootstrapFonts = pickFiles('bower_components/bootstrap-sass-official/assets/fonts/bootstrap', {
-    srcDir: '/',
-    destDir: '/assets/fonts'
-  });
-
-  app.import('bower_components/jquery-mockjax/dist/jquery.mockjax.js');
-
-  return mergeTrees([app.toTree(), bootstrapFonts]);
+  return app.toTree();
 };
